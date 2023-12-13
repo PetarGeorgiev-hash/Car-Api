8bf4cc0d1c26291430f623d162126e91ada98dbf
Petar Georgiev <petiyo07@abv.bg>
2023-12-13 17:47:50 +0200

    new branch created

--- src/users/user.entitiy.ts
+++ src/users/user.entitiy.ts
@@ -1,4 +1,12 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterRemove,
+  AfterUpdate,
+  AfterInsert,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class User {
@@ -9,5 +17,19 @@ export class User {
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted user with id', this.id);
+  }
+  @AfterUpdate()
+  logUpdate() {
+    console.log('updated', this.id);
+  }
+  @AfterRemove()
+  logRemove() {
+    console.log('removed user with id', this.id);
+  }
 }
